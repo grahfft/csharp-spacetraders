0dca968a1a71920642b3ee8d5518b82dacb45a0a
Dennis Lassell <dennis.lassell@gmail.com>
2025-02-17 13:12:40 -0500

    Finish setting up GraphQL Tutorial

--- GraphQlExampleService.Tests/UnitTest1.cs
+++ GraphQlExampleService.Tests/UnitTest1.cs
@@ -1,10 +1,42 @@
-﻿namespace GraphQlExampleService.Tests;
+﻿using System.Threading.Tasks;
+using HotChocolate;
+using HotChocolate.Execution;
+using HotChocolate.Types;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphQlExampleService.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public async Task Test1()
     {
+        // Arrange
+        var query = @"mutation addAuthor {
+                          addAuthor(input: { name: ""Schiller""}) {
+                            record {
+                              id
+                              name
+                            }
+                          }
+                        }";
 
+        var graphQlServer = new ServiceCollection()
+            .AddSingleton<Repository>()
+            .AddGraphQLServer()
+            .AddQueryType(q => q.Name("Query"))
+            .AddType<FirstQuery>()
+            .AddType<SecondQuery>()
+            .AddMutationType(m => m.Name("Mutation"))
+            .AddType<FirstMutation>()
+            .AddType<SecondMutation>();
+
+        // Act
+        var response = await graphQlServer.ExecuteRequestAsync(query);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response as OperationResult);
+        Assert.Null((response as OperationResult).Errors);
     }
 }
